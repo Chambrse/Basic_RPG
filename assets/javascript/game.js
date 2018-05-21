a1a5d90ff9fc164b8307627bf95d1ed100e0ca39
Chambrse <shaneechambry@gmail.com>
2018-05-20 23:07:16 -0700

    added favicon and attack button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,14 @@
 $(document).ready(function () {
 
+    /* Initialize variables. */
     var characterChooser = 0;
+
+    var playerCharacter;
+    var enemy;
+
+    var attackButton = $("<button>");
+    attackButton.addClass("btn btn-primary attackButton");
+    attackButton.text("Attack!");
 
     var donaldTrump = {
         "attack": 25,
@@ -8,11 +16,26 @@ $(document).ready(function () {
         "health": 180
     }
 
+    var sanders = {
+        "attack": 25,
+        "counter": 25,
+        "health": 180
+    }
+
 
     function selectCharacter(character) {
-/*         var charString = '#' + character;
- */        var playerCharacter = $('#' + character).detach();
+        playerCharacter = $('#' + character).detach();
         $("#gameSpace").append(playerCharacter);
+        $('#' + character).children().append(attackButton);
+    }
+
+    function selectEnemy(character) {
+        enemy = $('#' + character).detach();
+        $("#enemySpace").append(enemy);
+    }
+
+    function attack() {
+
     }
 
 
@@ -20,10 +43,17 @@ $(document).ready(function () {
         if (characterChooser == 0) {
             selectCharacter(this.id);
             characterChooser++;
+            $("#toolTip").text("Choose an opponent!")
         } else {
-
+            selectEnemy(this.id);
         }
+        $('#' + this.id).removeClass("cWrapper");
+        $('#' + this.id).children().removeClass("selectable");
     });
+
+    $(".attackButton").on("click", function () {
+
+    })
 
 
 
